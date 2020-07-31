b55c211c6e1b7484706d36be76e6342316db6145
stevekutz <stkutz@gmail.com>
2020-07-31 15:14:54 -0500

    created UserRegistrationForm

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -4,3 +4,17 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
      username = forms.CharField()
      password = forms.CharField(widget = forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+     password = forms.CharField(widget = forms.PasswordInput)     
+     password2 = forms.CharField(widget = forms.PasswordInput)
+
+     class Meta:
+          model = User
+          fields = ('username', 'first_name', 'last_name', 'email')
+
+     def clean_password2(self):
+          cd = self.cleaned_data
+          if cd['password'] != cd['password2']:     
+               raise forms.ValidationError('Passwords don\'t match ')
+          return cd['password2']
